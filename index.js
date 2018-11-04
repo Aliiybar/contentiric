d18d1d52c0b5701b84b8401d7deab8122be49e0d
Ali IYBAR <ali.iybar@gmail.com>
2018-11-04 18:38:11 +0000

    Configurations added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express');
+const config = require('config'); // Logger
 const morgan = require('morgan'); // Logger
+const express = require('express');
 
 const app = express()
 const port =  process.env.PORT || 3000
@@ -7,6 +8,13 @@ const port =  process.env.PORT || 3000
 app.use(express.urlencoded({ extended:true }));
 app.use(express.static('public'));
 app.use(express.json());
+
+// Configuration
+// export NODE_ENV=development 
+//console.log("Application Name : " + config.get('appName'));
+//console.log("Mail Server Name : " + config.get('mailServer.serverAddress'));
+// export contentiric_mail_password=Password123 // this env variable is mapped to mailServer.serverPassword in /config/custom-environment-variables file 
+//console.log("Mail Server Password : " + config.get('mailServer.serverPassword'));
 
 if(app.get('env') === 'development'){
     app.use(morgan('tiny'));
